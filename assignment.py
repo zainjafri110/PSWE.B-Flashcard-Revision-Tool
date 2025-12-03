49fef6956a27252d3954a5ad33218696c6abf387
zainjafri110 <zain.jafri1@education.nsw.gov.au>
2025-12-04 10:01:36 +1100

    added: what would you like to do

--- assignment.py
+++ assignment.py
@@ -1,3 +1,4 @@
 print("Welcome to the Revision Software!\nThis software allows you to create flash cards to help you memorise concepts for your exams")
 name = input("can I get your name? ")
 print("Welcome, " + name)
+input("What would you like to do? ")
