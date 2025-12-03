e2cc99cd8e3a25619f705ffaefc50706ccc927bb
syz14 <zain.jafri1@education.nsw.gov.au>
2025-12-04 09:53:29 +1100

    Create the file for the assessment

---
+++ assignment.py
@@ -0,0 +1,3 @@
+print("Welcome to the Revision Software!\nThis software allows you to create flash cards to help you memorise concepts for your exams")
+name = input("can I get your name? ")
+print("Welcome, " + name)
